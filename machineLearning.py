476d2e7e6956f7b6fb5da2423a2261c2a7ffe0ed
Daniel Handler <dthandler@gmail.com>
2014-11-03 15:06:46 +0100

    Rearrangement of python files

--- machineLearning.py
+++ machineLearning.py
@@ -14,41 +14,53 @@ import numpy as np
 from tmgsimple import TmgSimple
 import nltk
 
-# Import the tweets
-f = open('database.txt')
-raw = f.read()
-
-# List the words
-wordlist = [w.lower() for w in nltk.word_tokenize(raw)]
-
-# Make a sorted set of the words
-vocab = sorted(set(wordlist))
-
-# Print the set of words
-print vocab
-
-# Count occurences of one word
-word = "Sport"
-print wordlist.count(word)
-
-# Print occurences for every word in vocab
-for word in vocab:
-    o = wordlist.count(word)
-    print word, o
-
-
-
-
 
 """
-# Generate text matrix with TmgSimple from 02450
-textMatrix = TmgSimple(filename='testDatabase.txt', stopwords_filename='stopwords.txt')
-
-X = textMatrix.get_matrix(sort=True)
-attributeNames = textMatrix.get_words(sort=True)
-
-
-# Display the result
-print attributeNames
-print X
+Makes a set of words, and print every word and number of the words occurences in the tweets.
 """
+
+def wordCount( inputFile ):
+    # Import the tweets
+    raw = open(inputFile).read()
+
+    # List the words
+    wordlist = [w.lower() for w in nltk.word_tokenize(raw)]
+
+    # Make a sorted set of the words
+    vocab = sorted(set(wordlist))
+
+    # Print the set of words
+    #print vocab
+
+    # Make an output file
+    outputFile = open('data.txt', 'w')
+
+    # Count occurences of one word. Primarily for checking that it is the right tweets imported
+    word = "sport"
+    print wordlist.count(word)
+
+    # Print occurences for every word in vocab; And output to file 'data.txt'
+    for word in vocab:
+       o = wordlist.count(word)
+       #print word, o
+       outputFile.write(word)
+       outputFile.write('\t')
+       outputFile.write(str(o))
+       outputFile.write('\n')
+       
+
+"""
+Uses tmgSimple to make e textmatrix of the input tweets
+"""
+
+def makeTextMatrix( inputFile, stopwordFile ):
+    # Generate text matrix with TmgSimple from 02450
+    textMatrix = TmgSimple(filename=inputFile, stopwords_filename=stopwordFile)
+
+    X = textMatrix.get_matrix(sort=True)
+    attributeNames = textMatrix.get_words(sort=True)
+
+
+    # Display the result
+    print attributeNames
+    print X
---
+++ run.py
@@ -0,0 +1,15 @@
+"""
+Textprocessing of tweets
+
+Author:
+Daniel Handler
+s113446
+Technical University of Denmark
+"""
+
+import machineLearning as ml
+
+databaseFile = 'database.txt'
+stopwordFile = 'stopwords.txt'
+
+ml.wordCount(databaseFile)
