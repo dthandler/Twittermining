e8c92c3fddc4866275a07feb25646d715234df88
arthurdesj <arthur.desjardins@centrale-marseille.fr>
2014-12-01 13:07:52 +0100

    Deleted runTweetMining
    the run file is now shared for the 2 modules

--- runTweetMining.py
+++
@@ -1,9 +0,0 @@
-from tweetMining import TweetMining
-"""
-testing tweetMining - authentication keys are needed
-keys can be stored in a cfg file or hardcoded
-"""
-tweetminer = TweetMining()
-
-# search_tweets(hundreds of desired tweets, query string, output filename)
-tweetminer.search_tweets(2, 'sport', 'output.txt')
